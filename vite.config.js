0b0302f5e9483e63e17f91a1c97faa55e53dd3d1
jai_code <jsor1155@gmail.com>
2026-07-07 16:08:52 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // This ensures paths work perfectly on GitHub Pages
+  base: './', // Ensures all your game scripts and paths map perfectly on GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 })
